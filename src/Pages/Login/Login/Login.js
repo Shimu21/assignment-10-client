71c704c2527ad3317e264934fe65f3575006607d
shimu21ph <shimu21ph@gmail.com>
2022-04-18 23:18:37 +0600

    working on register

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
+
+    const navigateRegister = event => {
+        navigate('/register');
+    }
+
+
     return (
-        <div>
-            <h1>login form</h1>
-        </div>
+        <div className='container w-50 mx-auto'>
+            <Form onSubmit={handleSubmit}>
+                <h2 className='text-success text-center mt-2'>Please login</h2>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Text class;Name="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p>New to My Kitchen <span className='text-danger' onClick={navigateRegister}>Please register</span></p>
+
+        </div >
     );
 };
 
--- src/Pages/notFound/NotFound.js
+++ src/Pages/notFound/NotFound.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { EmojiSadIcon } from '@heroicons/react/solid'
+
+
 
 const NotFound = () => {
     return (
         <div>
+            <h1 className='text-5xl font-bold mt-8'>Page is sleeping</h1>
+            <EmojiSadIcon className='h-50 w-50 align-middle text-red-700'></EmojiSadIcon>
+
 
         </div>
     );
