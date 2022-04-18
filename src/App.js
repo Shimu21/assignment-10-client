0a19d182d26cb78fd27cd731f0a89d14490d7669
shimu21ph <shimu21ph@gmail.com>
2022-04-18 16:36:29 +0600

    react router and banner added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Login/Login/Login';
+import NotFound from './Pages/notFound/NotFound';
 
 function App() {
   return (
@@ -11,11 +14,15 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route Route path="/home" element={<Home Home ></Home >}></Route >
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-      </Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes >
       <Footer></Footer>
 
-    </div>
+    </div >
   );
 }
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
-            <h2>This is header</h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About Me</Nav.Link>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     );
 };
